b2114841584b07cbd4a0f7373558760a3a36ccd6
Alex Plong <alexplong@gmail.com>
2023-09-21 10:26:37 -0400

    refactor: clean up player and dealer prompts refactor: move detection and display of results to its own method refactor: move prompt to play again to own method

--- RB110/lesson_3/twenty_one.rb
+++ RB110/lesson_3/twenty_one.rb
@@ -8,18 +8,53 @@ def prompt(msg)
   puts "=> #{msg}"
 end
 
-def player_turn_prompt(player, dealer)
+def player_turn_prompt(player_hand, dealer_hand)
   system "clear"
-  puts "Dealer has: #{dealer[0][1]} and unknown card"
-  puts "You have: #{joinor(player)}"
+  puts "Dealer has: #{dealer_hand[0][1]} and unknown card"
+  puts "You have: #{joinor(player_hand)}"
 end
 
-def dealer_turn_prompt(player, dealer)
+def dealer_turn_prompt(player_hand, dealer_hand)
   system "clear"
-  puts "Dealer has: #{joinor(dealer)}"
-  puts "You have: #{joinor(player)}"
+  puts "Dealer has: #{joinor(dealer_hand)}"
+  puts "You have: #{joinor(player_hand)}"
 end
 
+def detect_result(dealer_hand, player_hand)
+  player_total = total(player_hand)
+  dealer_total = total(dealer_hand)
+
+  if player_total > 21
+    :player_busted
+  elsif dealer_total > 21
+    :dealer_busted
+  elsif dealer_total > player_total
+    :dealer
+  elsif dealer_total < player_total
+    :player
+  else
+    :tie
+  end
+end
+
+def display_result(dealer_hand, player_hand)
+  result = detect_result(dealer_hand, player_hand)
+
+  case result
+  when :player_busted
+    prompt "You busted! Dealer wins!"
+  when :dealer_busted
+    prompt "Dealer busted! You win!"
+  when :player
+    prompt "Congrats! You win!"
+  when :dealer
+    prompt "Dealer wins!"
+  when :tie
+    prompt "It's a tie!"
+  end
+end
+
+# refactor: improve with Array#product
 def initialize_deck
   deck = []
   SUITS.each do |suit|
@@ -30,15 +65,15 @@ def initialize_deck
   deck.shuffle
 end
 
-def deal_cards(deck, player, dealer)
+def deal_cards(deck, player_hand, dealer_hand)
   2.times do
-    player << deck.pop
-    dealer << deck.pop
+    player_hand << deck.pop
+    dealer_hand << deck.pop
   end
 end
 
-def joinor(player, delimiter=', ', word='and')
-  cards = player.map { |card| card[1] }
+def joinor(player_hand, delimiter=', ', word='and')
+  cards = player_hand.map { |card| card[1] }
 
   if cards.length > 2
     cards[-1] = "#{word} #{cards[-1]}"
@@ -83,46 +118,52 @@ def bust?(cards)
   total(cards) > 21
 end
 
+def play_again?
+  puts "Would you like to play again? (y or n)"
+  again = gets.chomp
+  again.downcase.start_with?('n')
+end
+
 loop do
-  dealer = []
-  player = []
+  dealer_hand = []
+  player_hand = []
 
   deck = initialize_deck
-  deal_cards(deck, player, dealer)
+  deal_cards(deck, player_hand, dealer_hand)
 
-  player_turn_prompt(player, dealer)
+  player_turn_prompt(player_hand, dealer_hand)
 
+  # player turn
   loop do
-    prompt("hit or stay?")
+    prompt("Would you like to hit or stay?")
     player_input = gets.chomp
-    hit(deck, player) if player_input.downcase.start_with?('h')
-    player_turn_prompt(player, dealer)
-    break if player_input.downcase.start_with?('s') || bust?(player)
+    hit(deck, player_hand) if player_input.downcase.start_with?('h')
+    player_turn_prompt(player_hand, dealer_hand)
+    break if player_input.downcase.start_with?('s') || bust?(player_hand)
   end
 
-  if bust?(player)
-    puts "Sorry you busted your hand. Play again? (y or n)"
-    again = gets.chomp
-    break if again.downcase.start_with?('n')
+  if bust?(player_hand)
+    puts "Sorry you busted your hand." 
+    break if play_again?
   else
     puts "Dealer turn!"
   end
 
+  # dealer turn
   loop do
-    dealer_turn_prompt(player, dealer)
-    break if total(dealer) >= 17 || bust?(dealer)
+    dealer_turn_prompt(player_hand, dealer_hand)
+    break if total(dealer_hand) >= 17 || bust?(dealer_hand)
     puts "Dealer will hit! Press any key to continue"
-    hit(deck, dealer)
+    gets
+    hit(deck, dealer_hand)
   end
 
-  puts "Dealer has #{total(dealer)} and you have #{total(player)}"
-  if total(player) < total(dealer) || total(dealer) > 21
-    puts "Congrats! You win! Play again? (y or n)"
-  elsif total(player) == 21 && total(dealer) == 21
-    puts "It's a tie! Play again? (y or n)"
-  else
-    puts "Sorry! You lose :( Play again? (y or n)"
-  end
-  play_again = gets.chomp
-  break if play_again.downcase.start_with?('n')
+  # both player and dealer stays - compare cards
+  puts "============"
+  puts "Dealer has #{joinor(dealer_hand)}, for a total of: #{total(dealer_hand)}"
+  puts "Player has #{joinor(player_hand)}, for a total of: #{total(player_hand)}"
+  puts "============"
+
+  display_result(dealer_hand, player_hand)
+  break if play_again?
 end
