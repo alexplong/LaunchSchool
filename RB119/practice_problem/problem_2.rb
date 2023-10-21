3fb7e47089b8bdd8cd22a30411322e6d0e4569c2
Alex Plong <alexplong@gmail.com>
2023-10-21 12:30:36 -0400

    Update: problem completed

--- RB119/practice_problem/problem_2.rb
+++ RB119/practice_problem/problem_2.rb
@@ -3,11 +3,56 @@
 # numbers in the array. If the array contains fewer than 5
 # elements, the method should return nil.
 
+
+
+## Problem
+# input: an array of integers
+# output: an integer
+#
+# explicit/implicit requirements:
+# - nil returned if size of array is fewer than 5 elements
+# - sum is minimum of 5 consecutive numbers
+# - integers in array can be positive or negative
+# clarifying questions?
+# -
+#
 # Examples:
+#
+# - The tests above should print "true".
+#
+## Data Structures
+# - Array: input
+# - Integer: output
+# - Array: of integers 
+#
+## Algorithm
+# - if size of `arr` is less than 5 return `nil`
+# - create a variable `output` and set to empty array
+# - given an array `arr` of integer elements
+# - iterate through the array
+#   - slice sub array from current element to next 5 elements and get sum of these 5 elements
+#   - append sum value to `output` array
+#   - break out of loop when return value element at current position + 5 is `nil`
+# - return the minimum value from `output`
 
-p minimum_sum([1, 2, 3, 4]) == nil
-p minimum_sum([1, 2, 3, 4, 5, 6]) == 15
-p minimum_sum([55, 2, 6, 5, 1, 2, 9, 3, 5, 100]) == 16
-p minimum_sum([-1, -5, -3, 0, -1, 2, -4]) == -10
+## Code with Intent
+def minimum_sum(arr)
+  return nil if arr.size < 5
 
-# The tests above should print "true".
+  output = []
+
+  arr.each_index do |idx|
+    output << arr[idx, 5].sum
+    break if arr[idx+5] == nil
+  end
+
+  output.min
+
+end
+
+p minimum_sum([1, 2, 3, 4]) #== nil
+p minimum_sum([1, 2, 3, 4, 5, 6]) #== 15
+p minimum_sum([55, 2, 6, 5, 1, 2, 9, 3, 5, 100]) #== 16
+p minimum_sum([-1, -5, -3, 0, -1, 2, -4]) #== -10
+
+# time 22 min
--- RB119/practice_problem/problem_3.rb
+++ RB119/practice_problem/problem_3.rb
@@ -3,7 +3,61 @@
 # in every third word converted to uppercase. Other characters
 # should remain the same.
 
-# Examples:
+
+
+## Problem
+# input: a string
+# output: a string
+#
+# explicit/implicit requirements:
+# - the 2nd character in every 3rd word > convert to uppercase
+# - order of characters remains the same - only case change for that particular word
+#
+# clarifying questions?
+# - Pass by value?
+#
+## Examples:
+# The tests should print "true".
+#
+## Data Structures
+# - String: input
+# - String: output - copy of original
+# - Integer: references 3rd word
+# - Array: word to split
+#
+## Algorithm
+# - Given a string as input `str`
+# - Create a constant third_word and set to ___
+# - Iterate to create copy and return transformed string
+#   - For each word in words - check if word is the 3rd word
+#     - If true, return word thats been split and rejoined where every second character in word transformed to uppercase
+#     - else just return word
+# - Return transformed string
+#
+## Code with Intent
+
+THIRD_WORD = 3
+
+def to_weird_case(str)
+  weird = str.split
+  weird.map!.with_index  do |word, idx|
+    if (idx + 1) % THIRD_WORD == 0
+
+      weird_word = word.chars
+      weird_word.map!.with_index do |char, idx2|
+        if idx2.odd?
+          char.upcase
+        else
+          char
+        end
+      end
+      weird_word.join
+    else
+      word
+    end
+  end
+  weird.join(' ')
+end
 
 p to_weird_case('Lorem Ipsum is simply dummy text of the printing') ==
                 'Lorem Ipsum iS simply dummy tExT of the pRiNtInG'
@@ -15,4 +69,4 @@ p to_weird_case(
   'Miss Mary Poppins word is supercalifragilisticexpialidocious') ==
   'Miss Mary POpPiNs word is sUpErCaLiFrAgIlIsTiCeXpIaLiDoCiOuS'
 
-# The tests above should print "true".
+## time 30+
