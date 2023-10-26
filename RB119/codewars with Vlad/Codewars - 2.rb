4fae61cfceca3562859389349550fa67fb255fdf
Alex Plong <alexplong@gmail.com>
2023-10-26 15:47:13 -0400

    Update: problem completed

---
+++ RB119/codewars with Vlad/Codewars - 2.rb
@@ -0,0 +1,63 @@
+# You are given array of integers, your task will be to count all pairs in that array and return their count.
+
+# Notes:
+
+#     Array can be empty or contain only one value; in this case return 0
+#     If there are more pairs of a certain number, count each pair only once. E.g.: for [0, 0, 0, 0] the return value is 2 (= 2 pairs of 0s)
+#     Random tests: maximum array length is 1000, range of values in array is between 0 and 1000
+
+# Examples
+
+# [1, 2, 5, 6, 5, 2]  -->  2
+
+# ...because there are 2 pairs: 2 and 5
+
+# [1, 2, 2, 20, 6, 20, 2, 6, 2]  -->  4
+
+# ...because there are 4 pairs: 2, 20, 6 and 2 (again)
+
+=begin
+- input: an array of integers
+- output: an integer
+
+requirements/questions?
+- count all pairs in the array\
+- return the count of all integers
+- return 0 if array is empty or only has 1 value
+- there can be more than 1 pair available
+
+algorithm
+- create a result variable and set to 0
+- given an array of integers
+- create a unique copy of input array
+- iterate through unique copy array
+  - count number of current integer in input array
+  - divide the count by 2
+  - add quotient value to result variable
+- return result
+
+=end
+
+def pairs(arr)
+  result = 0
+  uniq_arr = arr.uniq
+
+  uniq_arr.each do |n|
+    result += (arr.count(n) / 2)
+  end
+
+  result
+end
+
+
+p pairs([1, 2, 5, 6, 5, 2]) == 2
+p pairs([1, 2, 2, 20, 6, 20, 2, 6, 2]) == 4
+p pairs([0, 0, 0, 0, 0, 0, 0]) == 3
+p pairs([1000, 1000]) == 1
+p pairs([]) == 0
+p pairs([54]) == 0
+
+## time 12 min
+# explore possible options first before moving forward
+# take your Time
+# try things
