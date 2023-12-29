d0d7f51ee7cfaa9a484278bc5b6cfe7dcfbc91aa
Alex Plong <alexplong@gmail.com>
2023-12-29 12:10:40 -0500

    Update: Move update and Score class add

--- RB120/rps_project/rock_paper_scissors_game_v1.rb
+++ RB120/rps_project/rock_paper_scissors_game_v1.rb
@@ -1,36 +1,54 @@
 class Move
-  VALUES = ['rock', 'paper', 'scissors']
+  VALID_MOVES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 
-  def initialize(value)
-    @value = value
-  end
+  WINNING_MOVES = {
+    'rock' => ['scissors', 'lizard'],
+    'paper' => ['rock', 'spock'],
+    'scissors' => ['paper', 'lizard'],
+    'lizard' => ['paper', 'spock'],
+    'spock' => ['rock', 'scissors']
+  }
 
-  def rock?
-    @value == 'rock'
-  end
-
-  def paper?
-    @value == 'paper'
-  end
-
-  def scissors?
-    @value == 'scissors'
+  def initialize(choice)
+    @value = choice
   end
 
   def >(other_move)
-    (rock? && other_move.scissors?) ||
-      (paper? && other_move.rock?) ||
-      (scissors? && other_move.paper?)
+    WINNING_MOVES[@value].include?(other_move)
   end
 
   def <(other_move)
-    (rock? && other_move.paper?) ||
-      (paper? && other_move.scissors?) ||
-      (scissors? && other_move.rock?)
+    WINNING_MOVES[other_move.to_s].include?(@value)
   end
 
   def to_s
     @value
+  end
+end
+
+class Score
+  def initialize
+    @points = 0
+  end
+
+  def increment
+    @points += 1
+  end
+
+  def >=(num)
+    @points >= num
+  end
+
+  def ==(num)
+    @points == num
+  end
+
+  def reset
+    @points = 0
+  end
+
+  def to_s
+    @points.to_s
   end
 end
 
@@ -39,10 +57,9 @@ class Player
   attr_reader :score
 
   def initialize
-    @score = 0
+    @score = Score.new
     set_name
   end
-
 end
 
 class Human < Player
@@ -61,9 +78,9 @@ class Human < Player
     choice = nil
 
     loop do
-      puts "Please choose rock, paper, or scissors:"
+      puts "Please choose rock, paper, scissors, lizard, or spock:"
       choice = gets.chomp
-      break if Move::VALUES.include? choice
+      break if Move::VALID_MOVES.include? choice
       puts "Sorry, invalid choice."
     end
 
@@ -77,25 +94,26 @@ class Computer < Player
   end
 
   def choose
-    self.move = Move.new(Move::VALUES.sample)
+    self.move = Move.new(Move::VALID_MOVES.sample)
   end
 end
 
 # Game Orchestration Engine
 class RPSGame
-  attr_accessor :human, :computer
+  attr_accessor :human, :computer, :history
 
   def initialize
     @human = Human.new
     @computer = Computer.new
+    @history = { human.name => [], computer.name => [] }
   end
 
   def display_welcome_message
-    puts "Welcome to Rock, Paper, Scissors"
+    puts "Welcome to Rock, Paper, Scissors, Lizard, Spock"
   end
 
   def display_goodbye_message
-    puts "Thanks for playing Rock, Paper, Scissors. Good bye"
+    puts "Thanks for playing Rock, Paper, Scissors, Lizard, Spock. Good bye"
   end
 
   def display_moves
@@ -103,14 +121,33 @@ class RPSGame
     puts "#{computer.name} chose #{computer.move}"
   end
 
-  def display_winner
+  def determine_round_winner
     if human.move > computer.move
-      puts "#{human.name} won!"
+      human.score.increment
+      display_round_winner(human.name)
     elsif human.move < computer.move
-      puts "#{computer.name} won!"
+      display_round_winner(computer.name)
+      computer.score.increment
+    else
+      display_round_winner
+    end
+  end
+
+  def display_round_winner(winner = nil)
+    if winner
+      puts "#{winner} won!"
     else
       puts "It's a tie!"
     end
+  end
+
+  def display_score
+    puts "#{human.name}'s score: #{human.score}"
+    puts "#{computer.name}'s score: #{computer.score}"
+  end
+
+  def winner?
+    (human.score == 3) || (computer.score == 3)
   end
 
   def play
@@ -120,8 +157,11 @@ class RPSGame
       human.choose
       computer.choose
       display_moves
-      display_winner
-      break unless play_again?
+      determine_round_winner
+      display_score
+      if winner?
+        break unless play_again?
+      end
     end
     display_goodbye_message
   end
@@ -135,6 +175,9 @@ class RPSGame
       break if ['y', 'n'].include? answer.downcase
       puts "Sorry, please enter y or n."
     end
+
+    human.score.reset
+    computer.score.reset
 
     return true if answer == 'y'
     return false if answer == 'n'
